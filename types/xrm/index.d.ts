8a6c5ee8f8c61782409d520cd185ad8a104c5558
Gavin Canon-Phratsachack <GCanonPhratsachack@lamexfoods.us>
2025-04-02 08:57:10 -0500

    Undefined added to name property

--- types/xrm/index.d.ts
+++ types/xrm/index.d.ts
@@ -2478,7 +2478,7 @@ declare namespace Xrm {
         /**
          * The name
          */
-        name?: string;
+        name?: string | undefined;
 
         /**
          * Type of the entity.
